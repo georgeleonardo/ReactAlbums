c8e9e5ef7aad65937fef9ceccb726726f6070c96
George Leonardo <george.leonardo@booking.com>
2017-01-26 15:00:13 +0100

    First files

---
+++ index.ios.js
@@ -0,0 +1,16 @@
+//Import library
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// Create component
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
